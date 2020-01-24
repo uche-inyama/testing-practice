033c97fadd7bdc8a47ddbf9811c31721839ffe6d
uche-inyama <depoulet@yahoo.com>
2020-01-24 14:32:42 +0100

    implement calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const Calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  }
+};
+
+module.exports = Calculator;
